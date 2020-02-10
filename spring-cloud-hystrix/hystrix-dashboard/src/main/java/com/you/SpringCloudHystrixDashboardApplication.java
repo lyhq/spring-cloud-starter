39e9a69d7d362d648744d7577602b038e510e1fe
lyhq <947167863@qq.com>
2020-02-10 17:06:07 +0800

    Hystrix Dashboard commit

---
+++ spring-cloud-hystrix/hystrix-dashboard/src/main/java/com/you/SpringCloudHystrixDashboardApplication.java
@@ -0,0 +1,18 @@
+package com.you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @EnableHystrixDashboard: 启用 Hystrix Dashboard 功能
+ */
+@EnableHystrixDashboard
+@SpringBootApplication
+public class SpringCloudHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudHystrixDashboardApplication.class, args);
+	}
+
+}
